7be1737724d5465639cc41314a5d111e3c40be67
unknown <berhoua@gmail.com>
2023-09-16 13:43:09 +0100

    first commit

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php  
+// ================================================================= //
+// ================================================================= //
+// ====================== By Berhoua Agency =============== //
+// ================================================================= //
+// ================================================================= //
+
+$uri = 'https://www.Jkomedia.com';
+
+$wname = 'Jkomedia Premium Movies'; 
+
+$descrip = 'with Jkomedia you can watch any movie for free, no payments required';  
+
+$cpa_link = '#';  
+
+$ImdbApi = 'e0f9744821cd1c3f376954d8d10b085e';
+
+$comingsoon = '1';  //write 1 to display coming soon or 0 to hide 
+$related = '1'; // write 1 to display related movies or 0 to hide 
+
+?>
